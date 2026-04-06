37120b97cbce751f91e8670079b4ffaeb4086eb9
MarianaaHernandez <he_mariana@javeriana.edu.co>
2026-04-06 11:58:57 -0500

    Punto 2 Desarrollado

--- Caso_Hollywood.r
+++ Caso_Hollywood.r
@@ -30,3 +30,22 @@ sum(data$Genre == "Comedy")
 # Calculo del número de películas R
 sum(data$MPAA_D == 1)
 
+#PUNTO 2: ROI
+# A. Calcular el ROI por película
+data <- data %>%
+    mutate(ROI_US = (Total.U.S..Gross - Budget) / Budget)
+
+# Visualizar el ROI por película
+print(data %>%select(Movie, ROI_US))
+
+# Calcular la media del ROI
+mean(data$ROI_US)
+summary(data$ROI_US)
+## El ROI promedio de las peliculas es de aproximadamente 29,3%, lo que indica que las peliculas generan un retorno sobre la inversion positivo en promedio.
+
+# B. Intervalo de confianza del 95% para la media del ROI de las peliculas
+t.test(data$ROI_US)
+
+# C. Es mayor al 12%
+t.test(data$ROI_US, mu = 0.12, alternative = "greater")
+## El ROI promedio de las películas es significativamente mayor al 12% (p-value < 0.05), lo que sugiere que, en promedio, las películas generan un retorno sobre la inversión superior al 12%.
